634ef6fd6b769e4e885c821c40c107e4482e6781
brdx88 <brian.ic@outlook.com>
2025-12-10 15:41:15 +0700

    config driven asset

--- my_dagster_project/my_dagster_project/assets.py
+++ my_dagster_project/my_dagster_project/assets.py
@@ -1,8 +1,23 @@
-from dagster import asset
+from dagster import asset, Config
+
+class NumbersConfig(Config):
+    """
+    a "Config schema" called `NumbersConfig`.
+    It has 1 parameter: `count`, default value = `2`
+    """
+    count: int = 5
 
 @asset
-def numbers():
-    return [1,2,3,4,5]
+def numbers(config: NumbersConfig)  -> list[int]:
+    """
+    You inject config into the asset.
+    The asset uses that config to calculate a result.
+    """
+    return list(range(1, config.count + 1))
+
+# @asset
+# def numbers():
+#     return [1,2,3,4,5]
 
 @asset
 def sum_numbers(numbers):               # dagster detects the dependency automatically just because the input parameter name matches the other asset name. | no config, no DAG code, no wiring // super clean.
