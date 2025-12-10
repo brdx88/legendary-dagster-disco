d80a07056cb85eb7dcebaf94f2ef13b5c49d1de1
brdx88 <brian.ic@outlook.com>
2025-12-10 14:42:58 +0700

    second asset

--- my_dagster_project/my_dagster_project/assets.py
+++ my_dagster_project/my_dagster_project/assets.py
@@ -3,3 +3,7 @@ from dagster import asset
 @asset
 def numbers():
     return [1,2,3,4,5]
+
+@asset
+def sum_numbers(numbers):               # dagster detects the dependency automatically just because the input parameter name matches the other asset name. | no config, no DAG code, no wiring // super clean.
+    return sum(numbers)
